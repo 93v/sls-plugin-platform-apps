784c6a0bc081d2f91eafe739fe0c65a48459449f
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-05-04 18:37:21 +0400

    Clean start

--- types/platform-app.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IPlatformApp {
-  name?: string | null;
-  platform?: 'GCM' | null;
-  credential?: string | null;
-}
-
-export interface IPlatformAppsMap {
-  [key: string]: IPlatformApp;
-}
